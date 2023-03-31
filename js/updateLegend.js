f282eeecdbcf68bd11e8d17b367eb844829070d2
Omar <123294199+omarthisside@users.noreply.github.com>
2023-03-31 17:24:44 +0530

    Updates

--- js/updateLegend.js
+++ js/updateLegend.js
@@ -3,6 +3,7 @@
     var selectedKey = document.getElementById("selectedFilter");
     var legendCont = document.getElementById("legend-cont");
     var yellowLegendCont = document.getElementById("yellow-legend-cont");
+    var orangeLegendCont = document.getElementById("orange-legend-cont");
     var greenLegend = document.getElementById("green-legend");
     var orangeLegend = document.getElementById("orange-legend");
     var yellowLegend = document.getElementById("yellow-legend");
@@ -61,7 +62,7 @@
 
     if (selectedData == "legalStatus") {
       greenLegend.innerHTML = "Legal";
-      orangeLegend.innerHTML = "Partial Ban";
+      orangeLegendCont.style.display = "none";
       redLegend.innerHTML = "General Ban";
       yellowLegend.innerHTML = " ";
       yellowLegendCont.style.display = "none";}
